6109a8d9971d5e171fe5f1e0b97b37b0147d2603
slayher <layhertony@gmail.com>
2010-08-15 10:14:04 -0400

    first commit of new setup

---
+++ products/theme_genesis_slayher.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, vendor/cyanogen/products/cyanogen_sholes.mk)
+
+# Manually set the policy and PRODUCT_PACKAGES so we can tweak them 
+# depending on whether or not we build with Google. There is no way
+# to override PRODUCT_PACKAGES from an inheriting product yet.
+
+# Change 'defaulttheme' to the theme name
+# This will be the same name as the cyanogen_theme_defaulttheme.mk file
+# located in the same directory
+
+PRODUCT_NAME := theme_genesis_slayher
+
+# Here you need to rename defaulttheme to match the folder name as well
+PRODUCT_PACKAGE_OVERLAYS := vendor/slayher/overlay_genesis
+PRODUCT_LOCALES := en_US \
+		   hdpi
+
+
+
+
